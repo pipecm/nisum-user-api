d0ba433895d1ed8f1d736f594462704148278916
Felipe Cardenas <pipecm@gmail.com>
2024-08-12 14:28:09 -0400

    Add initial settings (first commit)

---
+++ src/main/java/com/nisum/userapi/NisumUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.userapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NisumUserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nisum/userapi/NisumUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NisumUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
